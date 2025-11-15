6fd9b97ba38c4a2e5f8352654f69800742add46a
Iasonas Ntamatis <iasonasntm06@gmail.com>
2025-11-15 02:55:11 +0200

    Finished adding some comments and notes again, P.S triangle still works

--- learning-log/day-03/src/main.cpp
+++ learning-log/day-03/src/main.cpp
@@ -3,12 +3,14 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+//Vertex Shader
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
+//Fragment Shader
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
@@ -27,6 +29,7 @@ int main(){
     //We are using core in order to avoid outdated functions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    //Verticies coordintates
     GLfloat verts[] = {
         -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
@@ -52,38 +55,56 @@ int main(){
     //Specify viewport
     glViewport(0, 0, 800, 800);
 
+    //Create vertex shader object and get it's reference
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    //Attach vertex shader source to the fragment shader object
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    //Compile the Vertex shader into machine code
     glCompileShader(vertexShader);
 
+    //Create fragment shader object and get it's reference
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    //Attach fragment shader source to the fragment shader object
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    //Compile the Fragment shader into machine code
     glCompileShader(fragmentShader);
 
+    //Create shader projgram and get it's reference
     GLuint shaderProgram = glCreateProgram();
 
+    //Attach the vertex and fragment shaders together to the shader program
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
 
+    //Link all the shaders together
     glLinkProgram(shaderProgram);
 
+    //Delete the vertex and fragment shader objects
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    //Create reference containers for the vertex array object and the vertex buffer object
     GLuint VAO, VBO;
     
+    //Generate the vertex array object and the vertex buffer object
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
+    //Make the VAO the current vertex array object by binding it
     glBindVertexArray(VAO);
 
+    //Bind the VBO specifying it's a GL_ARRAY_BUFFER
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //Introduce the verticies into the VBO
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
+    //Configure the Vertex attribute so that OpenGL knows how to reaad the VBO
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
                           3 * sizeof(float), (void*)0);
+    //Enable the Vertex attribute so that OpenGL knows to use it
     glEnableVertexAttribArray(0);
 
+    //Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -96,17 +117,24 @@ int main(){
 
     //main while loop
     while(!glfwWindowShouldClose(window)){
+        //Color of background
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        //clean the back buffer and assign the new color to it
         glClear(GL_COLOR_BUFFER_BIT);
+        //Tell opengl which shader program we want to use
         glUseProgram(shaderProgram);
+        // Bind the VAO so OpenGL knows to use it
         glBindVertexArray(VAO);
+        //Draw the triangle using the GL_TRIANGLES primitive
         glDrawArrays(GL_TRIANGLES, 0, 3);
+        //Swap the back buffer with the front buffer
         glfwSwapBuffers(window);
 
         //Take care of all GLFW events
         glfwPollEvents();
     }
 
+    //Delete all the objects we've created
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
---
+++ learning-log/day-04/notes.md
@@ -0,0 +1,24 @@
+## Third Day's notes:
+
+---
+
+## Log-1:
+
+Back at it with another unprofessional note, OpenGL has been quite the eye opening experience to how crazy the graphical world is. So far coding blindly with some text on the screen or using a game engine like unity have kept me safe, but that changed these past couple of days as i decided to try out this insane topic in the coding world. Indeed a crazy matter as today I only managed to make a triangle and that I'm too proud for what it actually is. Progress is being made I am waiting on uni assignments to cool down so that i can focus more on this rather interesting but insanely difficult task.
+
+---
+
+## EndLog 
+
+---
+
+## Useful Theory
+
+Today was also a no theory day, OpenGL really has been quite the new thing for me and it is definetly posing a major issue in terms of understanding what I'm doing. With time I am sure I will figure it out but for a first project it was indeed too much.
+
+---
+
+## Important Note
+
+Keep in mind I am using VS Code on Ubuntu 24.04 to write my scripts so things may not be perfect or even functional.
+As of right now switching to macOS on an apple device is my main goal so until then please be patient for any errors you may encounter.
